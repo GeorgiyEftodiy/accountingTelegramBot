ef6664112303a21dde416071fcbdb7810250d2d5
GeorgiyEftodiy <92368923+GeorgiyEftodiy@users.noreply.github.com>
2022-05-15 14:51:38 +0300

    Edit

--- callback.py
+++ callback.py
@@ -13,14 +13,14 @@ botDB = botDB('db.db')
 # Обрабатываем нажатие на inline-кнопку '100-тки'
 @dp.callback_query_handler(text='100-тки')
 async def detail_100(message: types.Message):
+    botDB.add_work(message.from_user.id, str(2))
     await bot.send_message(message.from_user.id, 'Введите количество сделанных мешков')
-    amu = message.text
-    await bot.send_message(message.from_user.id, 'Введите количество сделанных мешков' + amu)
 
 
 # Обрабатываем нажатие на inline-кнопку '50-тки'
 @dp.callback_query_handler(text='50-тки')
 async def detail_50(message: types.Message):
+    botDB.add_work(message.from_user.id, str(1))
     await bot.send_message(message.from_user.id, 'Введите количество сделанных мешков')
 
 
